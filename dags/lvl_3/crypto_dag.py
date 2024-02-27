aa2a4d3a84872ea45a8026a03eb7d61dd0a95173
a.belozersky <a.belozersky@vk.team>
2024-02-27 16:07:51 +0300

    fix crypto dag

--- dags/lvl_3/crypto_dag.py
+++ dags/lvl_3/crypto_dag.py
@@ -37,7 +37,7 @@ with DAG(
     dbt_run_crypto = DbtRunOperator(
         task_id='dbt_run_crypto',
         retries=0,
-        select='demo.lvl3',
+        select='demo.lvl_3',
     )
 
     dbt_test_crypto = DbtTestOperator(
